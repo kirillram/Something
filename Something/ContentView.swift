c36d3f987a69e9635439809fda0bfec95c9bace9
kirillram <kirill.bairamov@gmail.com>
2022-04-05 16:25:10 +0400

    Initial Commit

---
+++ Something/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Something
+//
+//  Created by Кирилл on 05.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Something/SomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingApp.swift
+//  Something
+//
+//  Created by Кирилл on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
